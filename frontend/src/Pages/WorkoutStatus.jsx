534dd7641f5f60e1e07599b6c7a6def990b6faab
komila <komikrish02@gmail.com>
2025-05-16 21:14:10 +0530

    add workout

--- frontend/src/Pages/WorkoutStatus.jsx
+++ frontend/src/Pages/WorkoutStatus.jsx
@@ -54,20 +54,7 @@ const WorkoutStatus = ({ user }) => {
         backgroundSize: 'cover',
         backgroundAttachment: 'fixed'
       }}>
-      <div className="space-y-4 flex justify-center flex-col items-center">
-        {workoutStatuses.map((status, index) => (
-          <div
-            key={index}
-            className="bg-white shadow-lg rounded-lg p-6 w-[600px] hover:shadow-xl transition-shadow duration-300"
-          >
-            <div className="flex justify-between items-center mb-4">
-              <div className="flex items-center space-x-4">
-                <div className="relative">
-                  <img
-                    src={status?.userProfile}
-                    alt="user"
-                    className="w-12 h-12 rounded-full object-cover"
-                  />
+      
                   <div className="absolute -bottom-1 -right-1 w-4 h-4 bg-green-500 rounded-full border-2 border-white"></div>
                 </div>
                 <div>
